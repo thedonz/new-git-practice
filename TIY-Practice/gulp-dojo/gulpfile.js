bf26a027e6c70fd2c9ff6745daad287ff506879d
Carlos Donze <xdonze@live.com>
2014-11-06 15:12:16 -0500

    @al-the-x coding dojo + Treehouse project updates

---
+++ TIY-Practice/gulp-dojo/gulpfile.js
@@ -0,0 +1,12 @@
+if ( condition ){
+  //code//;
+}
+
+function something(or, other){
+  //code//;
+}
+
+
+//GULP
+
+var gulp = require('gulp'); // gulp is the library, and it's assigned to the variable gulp.
