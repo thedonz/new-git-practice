ca64faf8ddaab776d68a3f4a0ee9f1b7216890bb
Carlos Donze <xdonze@live.com>
2014-11-06 16:34:51 -0500

    Next step in gulpfile

--- TIY-Practice/gulp-dojo/gulpfile.js
+++ TIY-Practice/gulp-dojo/gulpfile.js
@@ -1,39 +1,6 @@
-/*
-if ( condition ){
-  //code//;
-}
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
 
-function something(or, other){
-  //code//;
-}
-
-
-//GULP
-
-var gulp = require('gulp'); // gulp is the library, and it's assigned to the variable gulp.
-
-
-//To use:
-
-var gulp = require('gulp');
-
-gulp.task('somename', function(){
-    console.log('Word');
-});
-
-gulp.task('motha', function(){
-  console.log('To your motha');
-});
-
-// run more than one gulp confirm negative, for example to run multiple ones.
-
-// Default task
-
-gulp.task('default', [ 'somename', 'motha', 'somename' ]);
-
-*/
-
-var gulp = require('gulp');
-
-gulp.task('default', [ ], function(){
-});
+gulp.task('connect', function(){
+  connect.server({})
+})
