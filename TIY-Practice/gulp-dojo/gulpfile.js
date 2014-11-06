30fe6f10e290f47c522f4292a8648ee03fd46296
Carlos Donze <xdonze@live.com>
2014-11-06 15:48:47 -0500

    Messing with Gulpfile.js

--- TIY-Practice/gulp-dojo/gulpfile.js
+++ TIY-Practice/gulp-dojo/gulpfile.js
@@ -1,3 +1,4 @@
+/*
 if ( condition ){
   //code//;
 }
@@ -10,3 +11,29 @@ function something(or, other){
 //GULP
 
 var gulp = require('gulp'); // gulp is the library, and it's assigned to the variable gulp.
+
+
+//To use:
+
+var gulp = require('gulp');
+
+gulp.task('somename', function(){
+    console.log('Word');
+});
+
+gulp.task('motha', function(){
+  console.log('To your motha');
+});
+
+// run more than one gulp confirm negative, for example to run multiple ones.
+
+// Default task
+
+gulp.task('default', [ 'somename', 'motha', 'somename' ]);
+
+*/
+
+var gulp = require('gulp');
+
+gulp.task('default', [ ], function(){
+});
